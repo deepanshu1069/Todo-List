be82362b19c20217abedcdeb425beed02ff70826
deepanshu1069 <139270250+deepanshu1069@users.noreply.github.com>
2024-04-01 21:59:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+// let todoList=[];
+// DisplayItems();
+// function addTodo(){
+//     let inputElement=document.querySelector('#inputtext');
+//     let item = inputElement.value;
+//     todoList.push(item);
+//     inputElement.value='';
+    
+//     DisplayItems();
+    
+// }
+// function DisplayItems(){
+//     let Display=document.querySelector('#DisplayElement');
+//    for(let i=0; i<todoList.length; i++){
+//     Display.innerHTML= Display.innerHTML + todoList[i];
+//    }
+
+// }
+// DisplayItems();
+
+let todoList = [
+    {
+      item: 'Buy Milk',
+      dueDate: '4/10/2023'
+    },
+    {
+      item: 'Go to College',
+      dueDate: '4/10/2023'
+    }
+  ];
+  
+  displayItems();
+  
+  function addTodo() {
+    let inputElement = document.querySelector('#todo-input');
+    let dateElement = document.querySelector('#todo-date');
+    let todoItem = inputElement.value;
+    let todoDate = dateElement.value;
+    todoList.push({item: todoItem, dueDate: todoDate});
+    inputElement.value = '';
+    dateElement.value = '';
+    displayItems();
+  }
+  
+  function displayItems() {
+    let containerElement = document.querySelector('.todo-container');
+    let newHtml = '';
+    for (let i = 0; i < todoList.length; i++) {
+      let {item, dueDate} = todoList[i];
+      newHtml += `
+        <span>${item}</span>
+        <span>${dueDate}</span>
+        <button class='btn-delete' onclick="todoList.splice(${i}, 1);
+        displayItems();">Delete</button>
+      `;
+    }
+    containerElement.innerHTML = newHtml;
+  }
